a232f208d2c432352a1f5ae3b7927e16fae6da08
KenYang0531 <kentony0531@gmail.com>
2013-11-10 18:19:40 +0800

    123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123

---
+++ AirlineHW2/AirlineHW2Tests/AirlineHW2Tests.m
@@ -0,0 +1,34 @@
+//
+//  AirlineHW2Tests.m
+//  AirlineHW2Tests
+//
+//  Created by Ken Yang on 13/11/4.
+//  Copyright (c) 2013年 Ken Yang. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AirlineHW2Tests : XCTestCase
+
+@end
+
+@implementation AirlineHW2Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
